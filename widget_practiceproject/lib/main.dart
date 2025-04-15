2d13c5c9ec54da25b3d060f15e5fc007b70e86b7
Leedoseo <okok0840@naver.com>
2025-04-15 16:12:40 +0900

    practiceproject 사용자 정의 위젯 생성

---
+++ widget_practiceproject/lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(SplashScreen()); // SplashScreen 위젯을 첫 화면으로 지정
+}
+
+class SplashScreen extends StatelessWidget { // StatelessWidget 선언
+  @override
+  Widget build(BuildContext context) { // 위젯의 UI 구현
+    return MaterialApp( // 항상 최상단에 입력되는 위젯
+      home: Scaffold( // 항상 두 번째로 입력되는 위젯
+        body: Center( // 중앙 정렬 위젯
+          // 글자를 화면에 보여주는 위젯
+          child: Text("Splash Screen"),
+        ),
+      )
+    )
+  }
+}
