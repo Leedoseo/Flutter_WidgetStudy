21d66a4e291e6624e43eb2ac1aeb8248be57afe2
Leedoseo <okok0840@naver.com>
2025-04-21 21:45:50 +0900

    상태바 색상 변경

--- image_carousel/lib/screen/home_screen.dart
+++ image_carousel/lib/screen/home_screen.dart
@@ -1,10 +1,12 @@
 import "package:flutter/material.dart";
+import "package:flutter/services.dart"; // 상태바 색상 변경 위한 패키지
 
 class HomeScreen extends StatelessWidget { // 기본 홈 화면으로 사용할 HomeScreen 위젯 생성
   const HomeScreen({Key? key}) : super(key : key);
 
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light); // 상태바 색상 변경하는 코드
     return Scaffold(
       body: PageView( // PageView 추가
         children: [1, 2, 3, 4, 5] // 샘플 리스트 생성
