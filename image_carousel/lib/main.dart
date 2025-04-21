26c4b5089c16a873073c518f6f8d838a7a47c4ee
Leedoseo <okok0840@naver.com>
2025-04-21 21:40:39 +0900

    이미지 여백 삭제BoxFit.cover사용

--- image_carousel/lib/main.dart
+++ image_carousel/lib/main.dart
@@ -1,4 +1,4 @@
-import "package:image_carousel/screen/home_screen.dart";
+import "package:image_carousel/screen/home_screen.dart"; // HomeScreen을 홈 위젯으로 등록
 import 'package:flutter/material.dart';
 
 void main() {
--- image_carousel/lib/screen/home_screen.dart
+++ image_carousel/lib/screen/home_screen.dart
@@ -1,15 +1,17 @@
 import "package:flutter/material.dart";
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatelessWidget { // 기본 홈 화면으로 사용할 HomeScreen 위젯 생성
   const HomeScreen({Key? key}) : super(key : key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: PageView(
-        children: [1, 2, 3, 4, 5]
-        .map(
-            (number) => Image.asset("asset/img/image_$number.jpg"),
+      body: PageView( // PageView 추가
+        children: [1, 2, 3, 4, 5] // 샘플 리스트 생성
+        .map( // 위젯으로 매핑
+            (number) => Image.asset("asset/img/image_$number.jpg", // asset에 있는 이미지 로딩
+            fit: BoxFit.cover, // 부모ㅓ 위젯 전체를 덮는 선에서 최소한 크기로 조절
+            ),
         )
             .toList(),
       ),
