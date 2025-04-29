93799cdc00dbc00fd4e0752a5ef1622dac8b2503
Leedoseo <okok0840@naver.com>
2025-04-29 21:00:31 +0900

    디데이 및 커플 이미지 위젯 생성

--- d_day_app/lib/screen/home_screen.dart
+++ d_day_app/lib/screen/home_screen.dart
@@ -10,3 +10,17 @@ class HomeScreen extends StatelessWidget {
     );
   }
 }
+
+class _DDay extends StatelessWidget { // D-day 위젯 생성
+  @override
+  Widget build(BuildContext context) {
+    return Text("DDay Widget");
+  }
+}
+
+class _CoupleImage extends StatelessWidget { // 커플 이미지 위젯 생성
+  @override
+  Widget build(BuildContext context) {
+    return Text("Couple Image Widget");
+  }
+}
