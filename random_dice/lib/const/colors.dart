c04f221e393b0456faf388504abf4f557940748d
Leedoseo <okok0840@naver.com>
2025-05-07 15:15:16 +0900

    이미지 추가 + pubspec.yaml설정 + 사용할 색상 상수 추가

---
+++ random_dice/lib/const/colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const backgroundColor = Color(0xFF0E0E0E); // 배경색
+const primaryColor = Colors.white; // 메인 컬러
+final secondaryColor = Colors.grey[600]; // 서브 컬러 - [600]이라는 키값을 넣어서 런타임에 색상이 계산되기 때문에 const 사용 불가
+
