f9404aedfa5a402affa3bdb42aa19c739dc57c90
Leedoseo <okok0840@naver.com>
2025-05-08 18:27:34 +0900

    흔들기 기능 추가

--- random_dice/lib/screen/root_screen.dart
+++ random_dice/lib/screen/root_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:random_dice/screen/home_screen.dart';
 import 'package:random_dice/screen/settings_screen.dart';
+import "dart:math";
+import "package:shake/shake.dart";
 
 class RootScreen extends StatefulWidget {
   const RootScreen({Key? key}) : super(key: key);
@@ -14,6 +16,8 @@ class _RootScreenState extends State<RootScreen> with TickerProviderStateMixin {
 
   TabController? controller;
   double threshold = 2.7; // 민감도의 기본값 설정
+  int number = 1; // 주사위 숫자
+  ShakeDetector? shakeDetector;
 
   @override
 
@@ -21,8 +25,21 @@ class _RootScreenState extends State<RootScreen> with TickerProviderStateMixin {
     super.initState();
 
     controller = TabController(length: 2, vsync: this);
-
     controller!.addListener(tabListener); // 컨트롤러 속성이 변경될 때마다 실행할 함수 등록
+
+    shakeDetector = ShakeDetector.autoStart( // 흔들기 감지 즉시 시작
+      shakeSlopTimeMS: 100, // 감지 주기
+      shakeThresholdGravity: threshold, // 감지 민감도
+      onPhoneShake: onPhoneShake, // 감지 후 실행할 함수
+    );
+  }
+
+  void onPhoneShake() {
+    final rand = new Random();
+
+    setState(() {
+      number = rand.nextInt(5) + 1;
+    });
   }
 
   tabListener() { // 리스너로 사용할 함수
@@ -33,6 +50,7 @@ class _RootScreenState extends State<RootScreen> with TickerProviderStateMixin {
 
   dispose() {
     controller!.removeListener(tabListener); // 리스너에 등록한 함수 등록 취소
+    shakeDetector!.stopListening(); // 흔들기 감지 중지
     super.dispose();
   }
 
@@ -48,7 +66,7 @@ class _RootScreenState extends State<RootScreen> with TickerProviderStateMixin {
 
   List<Widget> renderChildren() {
     return [
-      HomeScreen(number: 1),
+      HomeScreen(number: number), // number 변수로 대체
       SettingsScreen(
         threshold: threshold,
         onThresholdChange: onThresholdChange,
