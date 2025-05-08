799c89c47f0e8446509203e0a3d35eccd158961c
Leedoseo <okok0840@naver.com>
2025-05-08 17:14:31 +0900

    탭바 구현+ TickerProvider 사용

--- random_dice/lib/screen/root_screen.dart
+++ random_dice/lib/screen/root_screen.dart
@@ -1,19 +1,35 @@
 import 'package:flutter/material.dart';
 
-class RootScreen extends StatelessWidget {
+class RootScreen extends StatefulWidget {
   const RootScreen({Key? key}) : super(key: key);
 
   @override
 
+  State<RootScreen> createState() => _RootScreenState();
+}
+
+class _RootScreenState extends State<RootScreen> with
+TickerProviderStateMixin { // TickerProviderStateMixin 사용 -> 애니메이션 효율 담당? 1프레임마다 애니메이션 실행
+  TabController? controller; // 사용할 TabController 선언
+
+  @override
+
+  void initState() {
+    super.initState();
+
+    controller = TabController(length: 2, vsync: this); // 컨트롤러 초기화
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
       body: TabBarView( // 탭바를 보여줄 위젯
+        controller: controller,
         children: renderChildren(),
       ),
-
       bottomNavigationBar: renderBottomNavigation(), // 아래 탭 내비게이션을 구현하는 파라미터
     );
   }
+
   List<Widget> renderChildren() {
     return [];
   }
@@ -22,3 +38,5 @@ class RootScreen extends StatelessWidget {
     return BottomNavigationBar(items: []); // 탭 내비게이션을 구현하는 위젯
   }
 }
+
+
