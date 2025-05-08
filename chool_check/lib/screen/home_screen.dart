2ac4084d46d30347db79663e01a2c30a4d8a0e11
Leedoseo <okok0840@naver.com>
2025-05-08 22:31:39 +0900

    Body에 지도 삽입

--- chool_check/lib/screen/home_screen.dart
+++ chool_check/lib/screen/home_screen.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class HomeScreen extends StatelessWidget {
+  static final LatLng companyLatLng = LatLng( // 지도 초기화 위치
+    37.5233273, // 위도
+    126.921252, // 경도
+  );
+
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
@@ -8,7 +14,12 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: renderAppBar(),
-      body: Text("Home Screen"),
+      body: GoogleMap(
+        initialCameraPosition: CameraPosition(
+          target: companyLatLng,
+          zoom: 16, // 확대 정도 (높을 수록 크게 보임)
+        ),
+      ),
     );
   }
 
