708f4f03d0cf9890197326a437b6d6975eed8c12
Leedoseo <okok0840@naver.com>
2025-05-13 16:53:53 +0900

    로고 불러오기 + 로고를 반환하는 함수 작성

--- ai_talk/lib/screen/home_screen.dart
+++ ai_talk/lib/screen/home_screen.dart
@@ -1,13 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:ai_talk/component/logo.dart'; // 로고 불러오기 
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen ({Key? key}) : super(key : key);
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
 
   @override
 
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Text("Home Screen"),
+    return const Placeholder();
+  }
+
+  Widget buildLogo() { // 가로 세로 패딩과 함께 Logo를 반환하는 _buildLogo()함수
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+      child: const Padding(
+        padding: EdgeInsets.only(bottom: 60.0),
+        child: Logo(),
+      ),
     );
   }
 }
